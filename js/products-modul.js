19416e16faaf8d0ce5e6822c79464f45f0c2ec92
Tonya-Shyva <toshash69@gmail.com>
2022-07-22 14:14:52 +0300

    products cards done

---
+++ js/products-modul.js
@@ -0,0 +1,20 @@
+// var card = document.querySelector('.products__card');
+// card.addEventListener('click', function () {
+//   card.classList.toggle('is-flipped');
+// });
+
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector('[.products__btn]'),
+//     closeModalBtn: document.querySelector('[data-modal-close]'),
+//     modal: document.querySelector('[data-moda-productsl]'),
+//   };
+
+//   refs.openModalBtn.addEventListener('click', toggleModal);
+//   refs.closeModalBtn.addEventListener('click', toggleModal);
+
+//   function toggleModal() {
+//     document.body.classList.toggle('modal-open');
+//     refs.modal.classList.toggle('is-hidden');
+//   }
+// })();
